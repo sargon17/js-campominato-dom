bbabb0dd562f1f5672dba7aae5d68a8aedd1a0cd
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-15 22:11:00 +0100

    Cleaned & Commented the JS File

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// -------------------------------- DOM ELEMENTS ------------------------------------
 const gameBoard = document.querySelector("#mt__game-box");
 const gameOverWindow = document.querySelector("#gameOverWindow");
 const gameOverText = document.querySelector("#gameOverText");
@@ -18,7 +19,9 @@ const playerEasyRecordPercentageDisplay = document.querySelector(
   "#playerEasyRecordPercentageDisplay"
 );
 const levelDisplay = document.querySelector("#levelDisplay");
+// -------------------------------- / DOM ELEMENTS ------------------------------------
 
+// -------------------------------- GENERAL VARIABLES DECLARATION ------------------------------------
 let bombs = [];
 let level = choseLevel.value;
 let playerCounter = 0;
@@ -30,6 +33,92 @@ let playerRecordEasy = 0;
 let playerRecordEasyPercentage = 0;
 let winningRatio = 0;
 let winningPercentage = 0;
+// -------------------------------- / GENERAL VARIABLES DECLARATION ------------------------------------
+
+// -------------------------------- FUNCTIONS ------------------------------------
+// --------------- Function that check the level chosed by player
+function levelChoice() {
+  level = parseInt(choseLevel.value);
+  switch (level) {
+    case 1:
+      levelDisplay.innerHTML = "Easy";
+      startGame(100);
+      break;
+    case 2:
+      levelDisplay.innerHTML = "Normal";
+      startGame(81);
+      break;
+    case 3:
+      levelDisplay.innerHTML = "Hard";
+      startGame(49);
+      break;
+
+    default:
+      startGame(100);
+
+      break;
+  }
+}
+// --------------- Function that start the game & all main process
+function startGame(cardsNumber) {
+  playerCounter = 0;
+  winningPercentage = 0;
+
+  //   Reset game on load & every restart
+  gameBoard.innerHTML = "";
+  // Reset bombs array
+  bombs = [];
+  //Create bombs array
+  for (let index = 0; index < 10; index++) {
+    bomb = Math.floor(Math.random() * cardsNumber + 1);
+    bombs.push(bomb);
+  }
+  console.log("for cheaters", bombs);
+  winningRatio = cardsNumber / (cardsNumber - 10);
+
+  // Create game cards
+  for (let index = 1; index <= cardsNumber; index++) {
+    let gameCard = document.createElement("div");
+    gameCard.classList.add("mt__game-card");
+    gameCard.classList.add(`mt__game-card--${cardsNumber}`);
+    gameCard.innerHTML = `<p class="mt__game-card__text d-inline-block">${index}</p>`;
+    gameCard.id = index;
+    gameBoard.appendChild(gameCard);
+
+    // Append on card games the event listener
+    gameCard.addEventListener("click", (target) => {
+      if (target.target.id) {
+        gameCampoMinato(target);
+      }
+    });
+  }
+}
+
+// --------------- Function that contain the game logic
+function gameCampoMinato(target) {
+  let id = parseInt(target.target.id);
+  // check if the clicked card is a bomb
+  if (bombs.includes(id)) {
+    // Display that You lose & update the records scores
+    target.target.classList.add("mt__bomb");
+    scoresRecord(level);
+    gameOverText.innerHTML = "Hai Perso";
+    gameOverText.classList.remove("gameOverText--win");
+    gameOverWindow.classList.remove("d-none");
+    playerScorePercentageDisplay.classList.remove("sidebar__percentage-over");
+  } else {
+    //Updates the score counter & Display counter
+    playerCounter++;
+    scoreChecker();
+    winningPercentage = playerCounter * winningRatio;
+    console.log(winningPercentage);
+    // update the game board
+    target.target.classList.add("mt__safe");
+    updateScoreDisplayer();
+    // check if the player has won
+    winChecker();
+  }
+}
 
 function updateScoreDisplayer() {
   playerScorePercentageDisplay.innerHTML = `${Math.floor(
@@ -94,81 +183,13 @@ function scoresRecord(level) {
   updateScoreDisplayer();
 }
 
-function gameCampoMinato(target) {
-  let id = parseInt(target.target.id);
-  if (bombs.includes(id)) {
-    target.target.classList.add("mt__bomb");
-    scoresRecord(level);
-    gameOverText.innerHTML = "Hai Perso";
-    gameOverText.classList.remove("gameOverText--win");
-    gameOverWindow.classList.remove("d-none");
-    playerScorePercentageDisplay.classList.remove("sidebar__percentage-over");
-  } else {
-    playerCounter++;
-    scoreChecker();
-    winningPercentage = playerCounter * winningRatio;
-    console.log(winningPercentage);
-    target.target.classList.add("mt__safe");
-    updateScoreDisplayer();
-    winChecker();
-  }
-}
+// -------------------------------- / FUNCTIONS ------------------------------------
 
-function levelChoice() {
-  level = parseInt(choseLevel.value);
-  switch (level) {
-    case 1:
-      levelDisplay.innerHTML = "Easy";
-      startGame(100);
-      break;
-    case 2:
-      levelDisplay.innerHTML = "Normal";
-      startGame(81);
-      break;
-    case 3:
-      levelDisplay.innerHTML = "Hard";
-      startGame(49);
-      break;
-
-    default:
-      startGame(100);
-
-      break;
-  }
-}
-
-function startGame(cardNumber) {
-  playerCounter = 0;
-  winningPercentage = 0;
-
-  gameBoard.innerHTML = "";
-  bombs = [];
-  for (let index = 0; index < 10; index++) {
-    bomb = Math.floor(Math.random() * cardNumber + 1);
-    bombs.push(bomb);
-  }
-  winningRatio = cardNumber / (cardNumber - 10);
-
-  console.log("for cheaters", bombs);
-
-  for (let index = 1; index <= cardNumber; index++) {
-    let gameCard = document.createElement("div");
-    gameCard.classList.add("mt__game-card");
-    gameCard.classList.add(`mt__game-card--${cardNumber}`);
-    gameCard.innerHTML = `<p class="mt__game-card__text d-inline-block">${index}</p>`;
-    gameCard.id = index;
-    gameBoard.appendChild(gameCard);
-
-    gameCard.addEventListener("click", (target) => {
-      if (target.target.id) {
-        gameCampoMinato(target);
-      }
-    });
-  }
-}
-
+// -------------------------------- ON LOAD FUNCTIONS ------------------------------------
 levelChoice();
+// -------------------------------- / ON LOAD FUNCTIONS ------------------------------------
 
+// -------------------------------- EVENT LISTENERS ------------------------------------
 gameOverRetryBtn.addEventListener("click", () => {
   levelChoice();
   gameOverWindow.classList.add("d-none");
@@ -178,3 +199,4 @@ keepPlayingBtn.addEventListener("click", () => {
 });
 
 choseLevelBtn.addEventListener("click", levelChoice);
+// -------------------------------- / EVENT LISTENERS ------------------------------------
