c759f576423695879095dadeea4e33329c76533c
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-16 18:27:41 +0100

    better responsivnes

--- script/script.js
+++ script/script.js
@@ -88,7 +88,6 @@ function startGame(cardsNumber) {
     let gameCard = document.createElement("div");
     gameCard.classList.add("mt__game-card");
     gameCard.classList.add(`mt__game-card--${cardsNumber}`);
-    gameCard.innerHTML = `<p class="mt__game-card__text d-inline-block">${index}</p>`;
     gameCard.id = index;
     gameBoard.appendChild(gameCard);
 
